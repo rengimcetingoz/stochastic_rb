0b0e22f7a6a376083431479b08386dfd5abaeadb
pepe-the-pew <florian.devillemeur@icloud.com>
2022-12-19 17:09:04 +0100

    Remove local path
    Clean all local path

--- integration/local.py
+++ integration/local.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('/Users/floriancarpentey/Documents/stochastic_rb/test/daily_returns_data.csv', index_col=0)
+data = pd.read_csv('./daily_returns_data.csv', index_col=0)
 
 sys.path.append(os.path.join(os.path.realpath(os.path.dirname(__file__)),"../"))
 
