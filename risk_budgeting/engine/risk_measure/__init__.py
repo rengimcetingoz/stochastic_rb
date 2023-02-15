c42cff86aa6c333d8b28a761d385780d9b0b4e15
rengimcetingoz <rengimcetingoz@gmail.com>
2023-02-15 23:31:26 +0100

    add-expected-shortfall-method

--- risk_budgeting/engine/risk_measure/__init__.py
+++ risk_budgeting/engine/risk_measure/__init__.py
@@ -1,2 +1,4 @@
 from risk_budgeting.engine.risk_measure.med_abs_dev import median_absolute_deviation_method
 from risk_budgeting.engine.risk_measure.volatility import volatility_method
+from risk_budgeting.engine.risk_measure.expected_shortfall import expected_shortfall_method
+
